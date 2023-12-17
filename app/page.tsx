e5b57ab4843185d19803019454b36b1fac185201
Kaan Koç <kock@mef.edu.tr>
2023-12-17 15:36:57 +0300

    fix: closes #4

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Image from 'next/image';
 import CustomText from '../components/CustomText';
+import IntroductionComponent from '../components/IntroductionComponent';
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <CustomText fontWeight={400} style={{ fontSize: 48 }}>
-        Hello World!
-      </CustomText>
+      <IntroductionComponent />
     </main>
   );
 }
