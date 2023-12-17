f713889246e0e8f05cf027a6238a944875aa4bb7
Kaan Koç <kock@mef.edu.tr>
2023-12-17 14:20:57 +0300

    fix: closes #1

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import CustomText from '../components/CustomText';
 
 export default function Home() {
   return (
@@ -6,6 +7,9 @@ export default function Home() {
       <button className='inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900'>
         Button
       </button>
+      <CustomText fontWeight={400} style={{ fontSize: 48 }}>
+        Hello World!
+      </CustomText>
     </main>
   );
 }
