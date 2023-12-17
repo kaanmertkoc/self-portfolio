9e31202fc821f2e838e0a01ef3ade3d3708b3951
Kaan Koç <kock@mef.edu.tr>
2023-12-17 14:07:18 +0300

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,42 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        darkBg: '#0D0D0D',
+        lightBg: '#FBFBFB',
+      },
+      darkMode: 'class',
     },
   },
-  plugins: [],
-}
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          primary: '#793ef9',
+          'primary-focus': '#570df8',
+          'primary-content': '#ffffff',
+          secondary: '#f000b8',
+          'secondary-focus': '#bd0091',
+          'secondary-content': '#ffffff',
+          accent: '#37cdbe',
+          'accent-focus': '#2aa79b',
+          'accent-content': '#ffffff',
+          neutral: '#2a2e37',
+          'neutral-focus': '#16181d',
+          'neutral-content': '#ffffff',
+          'base-100': '#0D0D0D',
+          'base-200': '#2a2e37',
+          'base-300': '#16181d',
+          'base-content': '#ebecf0',
+          info: '#66c6ff',
+          success: '#87d039',
+          warning: '#e2d562',
+          error: '#ff6f6f',
+        },
+      },
+      'light',
+    ],
+  },
+};
 export default config
