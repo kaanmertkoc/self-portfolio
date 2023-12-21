633cbb5a074718f0889ddbd1441be2d8728852ad
Kaan Koç <kock@mef.edu.tr>
2023-12-21 12:37:59 +0300

    feat: get to know me card add

---
+++ components/GetToKnowMeCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { AboutMeCard } from '../interfaces/AboutMeCard';
+
+type GetToKnowMeCardProps = {
+  aboutMe: AboutMeCard;
+};
+
+const GetToKnowMeCard = ({ aboutMe }: GetToKnowMeCardProps) => {};
+
+export default GetToKnowMeCard;
