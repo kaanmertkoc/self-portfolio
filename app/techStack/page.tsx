a91b294ef4258f404da40ca62263cce8dcd06922
Kaan Koç <kock@mef.edu.tr>
2024-01-01 16:00:10 +0300

    fix: closes #23

--- app/techStack/page.tsx
+++ app/techStack/page.tsx
@@ -1,4 +1,6 @@
 import CustomText from '../../components/CustomText';
+import TechStackCard from '../../components/TechStackCard';
+import { devDesignStack } from '../../utils/techStack';
 
 export default function TechStackPage() {
   return (
@@ -21,6 +23,21 @@ export default function TechStackPage() {
         }}
         className='mt-16 w-screen'
       />
+      <div className='w-[1024px] items-center mt-12'>
+        <CustomText
+          fontWeight={700}
+          style={{ fontSize: 48, color: '#808080', alignSelf: 'flex-start' }}
+        >
+          Dev & Design
+        </CustomText>
+        <div className='grid grid-cols-3 gap-6 mt-4'>
+          {devDesignStack.map((item, index) => (
+            <div key={index} className=''>
+              <TechStackCard techStack={item} />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
