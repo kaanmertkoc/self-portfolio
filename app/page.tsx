81ebc9dfce0e336759b457400d50749462ec7391
Kaan Koç <kock@mef.edu.tr>
2024-02-23 21:58:10 +0300

    fix: home screen mobile resopnsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import WorkTogether from '../components/HomeComponents/WorkTogether';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <main className='flex min-h-screen flex-col items-center justify-between md:p-24 p-4'>
       <IntroductionComponent />
       <SelectedWorks />
       <GetToKnowMe />
